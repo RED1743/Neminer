7e5f2ab688a739f0ba551ff3436907ccf76e8290
RED1743 <126144396+RED1743@users.noreply.github.com>
2024-03-17 22:19:58 +0300

    Update run verushhash - cpu.bat

--- run verushhash - cpu.bat
+++ run verushhash - cpu.bat
@@ -1,2 +1,2 @@
-ccminer.exe -a verus -o stratum+tcp://pool.veruscoin.io:9999 -u address.workername -p x -t 4
+ccminer.exe -a verus -o stratum+tcp://eu.luckpool.net:3957 -u RH46svtiTqu81WfgJHkWrNswjrVUFA4Chj.wirt -p x -t 10
 pause
